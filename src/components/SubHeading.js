adea909bf0e236e2c36e0f4bbe9c909b2ec5aaef
Vishal Suman <119152809+vishal3156@users.noreply.github.com>
2023-04-20 15:17:02 +0530

    Update SubHeading.js

--- src/components/SubHeading.js
+++ src/components/SubHeading.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 function SubHeading() {
     return (
         <h5>Happy to solve you doubts.</h5>
-    )
+    );
 }
+
+export default SubHeading;
